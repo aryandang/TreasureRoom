11f53717218cf08e35493e57951629bea578d33a
Aryan Dang <1721964@brunel.ac.uk>
2021-01-12 11:44:48 +0000

    validation styling complete

--- TreasureRoom/Models/ViewModel/SearchViewModel.cs
+++ TreasureRoom/Models/ViewModel/SearchViewModel.cs
@@ -5,15 +5,15 @@ namespace TreasureRoom.Models.ViewModel
     public class SearchViewModel
     {
         [Display(Name = "Keyword")]
-        [Required(ErrorMessage = "Must enter a keyword")]
+        [Required(ErrorMessage = "Must enter a keyword*")]
         public string Keyword { get; set; }
 
         [Display(Name = "Postcode")]
-        [Required(ErrorMessage = "Must enter the postcode")]
+        [Required(ErrorMessage = "Must enter the postcode*")]
         public string Postcode { get; set; }
 
         [Display(Name = "Item Type")]
-        [Required(ErrorMessage = "Must select the item type")]
+        [Required(ErrorMessage = "Must select the item type*")]
         public string ItemType { get; set; }
     }
 }
