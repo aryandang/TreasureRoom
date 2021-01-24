aab4d36f2bdc5c0f95dcbb2cc9c1b99fa290c077
Aryan Dang <1721964@brunel.ac.uk>
2021-01-24 10:08:13 +0000

    Post page set up in progress

---
+++ TreasureRoom/Models/PostContentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web.Models;
+
+namespace TreasureRoom.Models
+{
+    public class PostContentModel : ContentModel
+    {
+        public PostContentModel(IPublishedContent content) : base(content)
+        {
+
+        }
+
+    }
+}
