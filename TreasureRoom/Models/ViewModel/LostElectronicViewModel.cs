a8f4a11effaaa95796e5e007ba64eb0410ef7aaf
Aryan Dang <1721964@brunel.ac.uk>
2021-02-04 13:57:37 +0000

    Checkboxes implemented for consents

--- TreasureRoom/Models/ViewModel/LostElectronicViewModel.cs
+++ TreasureRoom/Models/ViewModel/LostElectronicViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 using System.Web;
 
 namespace TreasureRoom.Models.ViewModel
@@ -97,12 +98,12 @@ namespace TreasureRoom.Models.ViewModel
         public string EmailAddress { get; set; }
 
         [Display(Name = "My age is above 18 and all data I have filled is honestly filled.")]
-        [Required(ErrorMessage = "Only 18 above and honest data is permitted.")]
-        public bool? Age18AboveAndHonesty { get; set; }
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Only 18 above and honest data is permitted.*")]
+        public bool Age18AboveAndHonesty { get; set; }
 
 
         [Display(Name = "I consent to usage of my personal data by Treasure Room.")]
-        [Required(ErrorMessage = "You must consent that your personal data will be used by Treasure Room")]
-        public bool? ConsentToData { get; set; }
+        [Range(typeof(bool), "true", "true", ErrorMessage = "You must consent that your personal data will be used by Treasure Room*")]
+        public bool ConsentToData { get; set; }
     }
 }
--- TreasureRoom/Models/ViewModel/LostWalletViewModel.cs
+++ TreasureRoom/Models/ViewModel/LostWalletViewModel.cs
@@ -98,12 +98,12 @@ namespace TreasureRoom.Models.ViewModel
 
 
         [Display(Name = "My age is above 18 and all data I have filled is honestly filled.")]
-        [Required(ErrorMessage = "Only 18 above and honest data is permitted.")]
-        public bool? Age18AboveAndHonesty { get; set; }
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Only 18 above and honest data is permitted.*")]
+        public bool Age18AboveAndHonesty { get; set; }
 
 
         [Display(Name = "I consent to usage of my personal data by Treasure Room.")]
-        [Required(ErrorMessage = "You must consent that your personal data will be used by Treasure Room")]
-        public bool? ConsentToData { get; set; }
+        [Range(typeof(bool), "true", "true", ErrorMessage = "You must consent that your personal data will be used by Treasure Room*")]
+        public bool ConsentToData { get; set; }
     }
 }
